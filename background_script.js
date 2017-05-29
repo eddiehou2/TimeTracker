bd5d45ec6591df5d2d4013702461f90d6845b54f
Eddie Hou <dearestatut@hotmail.com>
2017-05-29 12:59:36 -0700

    Completed background js working

--- background_script.js
+++ background_script.js
@@ -1,16 +1,45 @@
-chrome.extension.onRequest.addListener(function(request, sender) {
-  switch (request.message) {
-    case "checkActive":
-      chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        var tab = tabs[0];
-        var pathArray = tab.url.split( '/' );
-        var protocol = pathArray[0];
-        var host = pathArray[2];
-        var url = protocol + '//' + host;
-        alert(url);
-      });
-      break;
-    default:
+var UserTimeData = {};
 
-  }
-});
+function checkActiveTab() {
+  console.log("checkActiveTab");
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    var tab = tabs[0];
+    if (tab) {
+      var pathArray = tab.url.split( '/' );
+      var protocol = pathArray[0];
+      var host = pathArray[2];
+      var url = protocol + '//' + host;
+      console.log("tab is not null");
+      if (UserTimeData.hasOwnProperty(url)) {
+        UserTimeData[url] += 15;
+        console.log("Exist");
+      }
+      else {
+        UserTimeData[url] = 15;
+        console.log("Does not exist");
+      }
+      chrome.storage.sync.set({ "TTData": UserTimeData }, function(){
+          console.log("Successfully set TTData");
+      });
+      for (var i in UserTimeData) {
+        console.log("URL: " + i + " // Time: " + UserTimeData[i]);
+      }
+    }
+  });
+  setTimeout(checkActiveTab,10000);
+}
+
+function initSetup() {
+  chrome.storage.sync.get("TTData", function(items) {
+    if (items["TTData"]) {
+      UserTimeData = items["TTData"];
+      console.log("[INFO] Loaded previous storage data.");
+    }
+    else {
+      console.log("[INFO] No storage items.")
+    }
+    checkActiveTab();
+  });
+}
+
+initSetup();
--- popup.js
+++ popup.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-  
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    var tab = tabs[0];
+    var pathArray = tab.url.split( '/' );
+    var protocol = pathArray[0];
+    var host = pathArray[2];
+    var url = protocol + '//' + host;
+    document.getElementById('url').innerHTML = url;
+  });
 }, false);
